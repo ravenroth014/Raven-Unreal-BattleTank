8d4ed0e34823e7f80a9da2f21fbf72ad3f635ee6
Atid Puwatnuttasit <atid.puw@gmail.com>
2018-02-27 14:12:20 +0700

    Use clamp to limit pitch value.

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -20,7 +20,7 @@ public:
 	
 private:
 	UPROPERTY(EditAnywhere, Category = Setup)
-		float MaxDegreesPerSecond = 20; // Sensible Default
+		float MaxDegreesPerSecond = 5; // Sensible Default
 
 	UPROPERTY(EditAnywhere, Category = Setup)
 		float MaxElevationDegree = 35; 
