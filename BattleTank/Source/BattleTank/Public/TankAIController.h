b4ada62b649db6307d0199c19c2d453051daf542
Atid Puwatnuttasit <atid.puw@gmail.com>
2018-03-02 14:20:27 +0700

    Dissecting RequestDirectMove().

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -18,8 +18,8 @@ class BATTLETANK_API ATankAIController : public AAIController
 	
 public:
 
-	//virtual void BeginPlay() override;
-
 	virtual void Tick(float DeltaSeconds) override;
-	
+
+	// How close can the AI tank get to 
+	float AcceptanceRadius = 3000;
 };
